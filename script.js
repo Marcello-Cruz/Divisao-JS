f6724b52d93677dcaad4bb5537bc1269f3fb9099
MarcelloB2 <marcellinhopastelzinho@gmail.com>
2022-05-26 21:44:30 -0300

    correçao final

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const numerador = Number(prompt("Digite o numerador"));
 const denominador = Number(prompt("Digite o denominador"));
+alert ('Digite o numerador');
+alert ('Digite o denominador')
 
 if (Object.is(numerador, NaN) || Object.is(denominador, NaN) ){
     alert("Informe apenas números");
@@ -7,5 +9,5 @@ if (Object.is(numerador, NaN) || Object.is(denominador, NaN) ){
     alert("Não é possível dividir por zero.");
 }else{
     const divisao = Number(numerador / denominador);
-    alert(`${numerador} dividido por ${denominador} é ${divisao}.`)
+    alert(`${numerador} dividido por ${denominador} é igual a ${divisao}.`)
 }
